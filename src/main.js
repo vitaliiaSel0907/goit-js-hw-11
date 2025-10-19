ab1cfc4eb4f0b12b1e5e11ccf7557670a56a2627
Vitaliia <vitaliia0907@gmail.com>
2025-10-19 15:13:10 +0200

    Modified file html and location correcte main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { getImagesByQuery } from './pixabay-api.js';
-import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
